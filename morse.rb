ef1831f414b3106542bafe9e8d7c5d008a650f8a
Daphineatim <hopedaphine19@gmail.com>
2022-12-22 12:19:03 +0300

    Modify morse.rb

--- morse.rb
+++ morse.rb
@@ -44,6 +44,22 @@ def decode_char(char)
 end
 puts decode_char('-.-')
 
+# decode a word
+def decode_word(word)
+    @result = ''
+    word.split.each { |i| @result.concat(@morse_code[i]) }
+    @result
+  end
+  puts decode_word('-.. ---')
 
-
-
+# decode a sentence 
+def decode(message)
+    @sentence = message.split('   ')
+    @decoded_sentence = ''
+    @sentence.each do |n|
+      @decoded_sentence += "#{decode_word(n)} "
+    end
+    @decoded_sentence.strip
+  end
+ puts decode("-- -.--   -. .- -- .")
+ puts decode(' .-  -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
